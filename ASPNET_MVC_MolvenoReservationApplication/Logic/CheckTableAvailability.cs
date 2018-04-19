489f1f385f94afb64a4b25e7e1474448470d9d77
George II <georgevassiliadis@hotmail.com>
2018-04-19 16:30:11 +0200

    Added 'Logic' folder and in it a class for implementing the needed methods for reservation availability checking.

---
+++ ASPNET_MVC_MolvenoReservationApplication/Logic/CheckTableAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_MVC_MolvenoReservationApplication.Logic
+{
+    public class CheckTableAvailability
+    {
+        public bool CheckDateAvailability(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckTime(DateTime time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPartySize(int partySize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
