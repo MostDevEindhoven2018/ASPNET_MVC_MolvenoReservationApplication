5b1a9ca9cb0947c82351240fab9b63301c5663d7
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-04-21 18:02:29 +0200

    Changed time. Makes list (not array anymore) that checks is current reservation falls before OR after current reservation. True: do nothing; false add record to list. List empty no overlap.
    Have to do per table if the list contains records.
    
    Still have to do number of people

--- ASPNET_MVC_MolvenoReservationApplication/Logic/CheckTableAvailability.cs
+++ ASPNET_MVC_MolvenoReservationApplication/Logic/CheckTableAvailability.cs
@@ -11,68 +11,69 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Logic
 {
     public class CheckTableAvailability
     {
+        // The database
         MyDBContext _dbContextobj;
-        
-        Reservation[] listReservationDate = new Reservation[100];
-        Reservation[] listReservationDateTime = new Reservation[100];
 
+        // Make lists
+        // listReservationDate: list with existing reservations that have the same reservation date as the current reservation
+        // listReservationDateTime: list with existing reservations that have the same reservation date and time as the current reservation
+        List<Reservation> listReservationDate = new List<Reservation>();
+        List<Reservation> listReservationDateTime = new List<Reservation>();
+
+        /// <summary>
+        /// From database Reservations gets the existing reservations that have the same date as the current reservation.
+        /// </summary>
+        /// <param name="date">The current reservation date</param>
+        /// <returns>Returns a bool. True: No existing reservations on the current reservation date. False: Existing reservations on the current reservation date</returns>
         public bool CheckDateAvailability(DateTime date)
-        {            
+        {
+            // Select records of the existing reservations that have the same reservation date as the current reservation
             var query = from res in _dbContextobj.Reservations
                         where res._resArrivingTime == date
                         select res;
 
-            listReservationDate = query.ToArray();
+            // Saves the seleted records from the database to the list "listReservationDate"
+            listReservationDate = query.ToList();
 
-            if (listReservationDate.First() == null)
+            // Checks if the list is empty, if it is empty, there a no reservations for that day and returns true (can make a reservation), or else returns false
+            if (listReservationDate.Count == 0)
             {
                 return true;
             }
             else
             {
-
                 return false;
             }
-
-
-
-
         }
 
-        /// <summary
-        /// Checks if current reservation time is not already reserved by an excisting reservation
+        /// <summary>
+        /// From CheckDateAvailability method's list of overlapping reservations dates from existing reservations and current reservation, checks if there are overlapping reservation times
         /// </summary>
-        /// <param name="time">Type=DateTime variable is the arrival time of the current reservation</param>
-        /// <returns>Returns if there is a bool. True: no overlap of current reservation time with excisting reservations False: Overlap of current reservation time with excisting reservations </returns>
+        /// <param name="time"></param>
+        /// <returns></returns>
         public bool CheckTime(DateTime time)
         {
 
-
-            for (int i = 0; i < 100; i++)
+            foreach (Reservation res in listReservationDate)
             {
-                DateTime prevResLeavingTime = listReservationDate[i].
-                    _resArrivingTime.AddHours(3);
+                DateTime prevResLeavingTime = res._resArrivingTime.AddHours(3);
                 DateTime currentResLeavingTime = time.AddHours(3);
 
-                //if the time our guest is arraving 
-                //is earlier than the previous guest leaving time
-                if (time < listReservationDate[i]._resArrivingTime)
+                //if this is true do nothing: the current reservation is before the existing reservation OR the current reservation is after the existing reservation
+                // the current reservation is before the existing reservation == time < res._resArrivingTime && currentResLeavingTime <= res._resArrivingTime
+                // the current reservation is after the existing reservation == time >= prevResLeavingTime && currentResLeavingTime > prevResLeavingTime
+                // if this is false, then add record to the listReservationDateTime
+                if ((time < res._resArrivingTime && currentResLeavingTime <= res._resArrivingTime) || (time >= prevResLeavingTime && currentResLeavingTime > prevResLeavingTime))
+                { }
+                else
                 {
-                    if (time < prevResLeavingTime)
-                    {
-                        if (currentResLeavingTime > listReservationDate[i + 1]._resArrivingTime)
-                        {
-                            listReservationDateTime[i]= listReservationDate[i];
-
-                        }
-
-                    }
-
+                    listReservationDateTime.Add(res);
                 }
 
             }
 
-            if (listReservationDateTime.First() == null)
+            // If listReservationDateTime has no records, then overlapping date and time of current reservations with existing ones
+            if (listReservationDateTime.Count() == 0)
             {
                 return true;
             }
@@ -81,8 +82,6 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Logic
 
                 return false;
             }
-
-            
         }
 
         public bool CheckPartySize(int partySize)
