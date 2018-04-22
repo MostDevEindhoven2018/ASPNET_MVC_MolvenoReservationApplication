0ac0a0f940de1eca22392ccf0a7a78377b4a3539
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-04-22 11:33:20 +0200

    Still working on it. Testing now if anything works

--- ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationController.cs
+++ ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationController.cs
@@ -19,10 +19,9 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
             _dbContextobj = _context;
             _dbContextobj.Database.EnsureCreated(); //Checks if a database is already created, if not it creates it
         }
-
-
-
-        public IActionResult CheckAvailability(DateTime _arrivingDateTime, int _partySize, TableAreas _tableArea)
+        
+        
+        public IActionResult CheckAvailability(/*DateTime _arrivingDateTime, int _partySize, TableAreas _tableArea*/)
         {
             /* tableId | date | time
              * 
@@ -36,8 +35,105 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
              * 
              * (1)Check if the specific table has the required capacity.
              */
+             
+            /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            /// FILL DATABASE START
 
-             return View();
+            var table1 = new Table
+            {
+                // Can't set the ID, or else error:: SqlException: Cannot insert explicit value for identity column in table 'Tables' when IDENTITY_INSERT is set to OFF.
+                //TableID = 1,
+                _tableCapacity = 4,
+                MyProperty = TableAreas.Window
+            };
+
+            var res1 = new Reservation
+            {
+                //ReservationID = 1,
+                _resTable = table1,
+                 _resPartySize=3,
+                _resArrivingTime= new DateTime (2018,04,27,12,00,00),
+                _resHidePrices = false,
+                _resComments = ""
+            };
+
+            var table2 = new Table
+            {
+                //TableID = 2,
+                _tableCapacity = 6,
+                MyProperty = TableAreas.Fireplace
+            };
+
+            var res2 = new Reservation
+            {
+                //ReservationID = 2,
+                _resTable = table2,
+                _resPartySize = 5,
+                _resArrivingTime = new DateTime(2018, 04, 27, 14, 00, 00),
+                _resHidePrices = false,
+                _resComments = ""
+            };
+
+            var res3 = new Reservation
+            {
+                //ReservationID = 3,
+                _resTable = table2,
+                _resPartySize = 6,
+                _resArrivingTime = new DateTime(2018, 04, 27, 17, 00, 00),
+                _resHidePrices = false,
+                _resComments = ""
+            };
+
+            var table3 = new Table
+            {
+                //TableID = 3,
+                _tableCapacity = 2,
+                MyProperty = TableAreas.Lake
+            };
+
+            var res4 = new Reservation
+            {
+                //ReservationID = 4,
+                _resTable = table3,
+                _resPartySize = 2,
+                _resArrivingTime = new DateTime(2018, 04, 27, 15, 00, 00),
+                _resHidePrices = false,
+                _resComments = ""
+            };
+
+            //_dbContextobj.Tables.Add(table1);
+            //_dbContextobj.Reservations.Add(res1);
+            //_dbContextobj.Tables.Add(table2);
+            //_dbContextobj.Tables.Add(table3);
+            //_dbContextobj.Reservations.Add(res2);
+            //_dbContextobj.Reservations.Add(res3);
+            //_dbContextobj.Reservations.Add(res4);
+            //_dbContextobj.SaveChanges();
+
+            /// FILL DATABASE END
+            /// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            /// 
+
+            DateTime Curr_res_date = new DateTime(2018, 04, 27, 15, 00, 00);
+
+
+            //CheckTableAvailability Checkings = new CheckTableAvailability();
+            //Checkings.CheckDateAvailability(Curr_res_date);
+            //Checkings.CheckTime(Curr_res_date);
+            ////Checkings.CheckTimePerTable();
+
+            //List <Reservation> list = Checkings.listReservationDate;
+
+
+            //return View(list);
+
+            var query = from res in _dbContextobj.Reservations
+                        where res._resArrivingTime == Curr_res_date
+                        select res;
+            // Saves the seleted records from the database to the list "listReservationDate"
+            
+            return View (query.ToList());
+
         }
 
        
