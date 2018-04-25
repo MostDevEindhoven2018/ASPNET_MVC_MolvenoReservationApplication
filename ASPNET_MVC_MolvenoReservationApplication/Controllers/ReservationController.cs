a2ebf84b368beb7379195a232706df8cf2102fae
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-04-25 14:13:03 +0200

    dont remember again

--- ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationController.cs
+++ ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationController.cs
@@ -30,13 +30,6 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
             //return View("CreateReservationView");
         }
 
-        
-
-        
-
-        
-        
-
         public IActionResult CheckAvailability(DateTime _arrivingDateTime, int _partySize/*, TableAreas _tableArea*/)
         {
             /* tableId | date | time
@@ -54,19 +47,21 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
 
             DateTime Curr_res_arr = new DateTime(2018, 04, 27, 15, 0, 0);
 
-            CheckTableAvailability Checkings = new CheckTableAvailability(_dbContextobj);
-            
-            List< Reservation> list1= Checkings.CheckDateAvailability(Curr_res_arr);
-                        
+            //CheckTableAvailability Checkings = new CheckTableAvailability(_dbContextobj);
+
+            //List< Reservation> list1= Checkings.CheckDateAvailability(Curr_res_arr);
+
             ////TEST: to see if database can be reached
             ////return View(list);
             //// https://stackoverflow.com/questions/19238413/how-to-display-foreign-key-values-in-mvc-view
-            //var query = from res in _dbContextobj.Reservations.Include(p=>p._resTable)
-            //            where res._resArrivingTime.Date == Curr_res_arr.Date
-            //            select res;
-            //// Saves the seleted records from the database to the list "listReservationDate"
 
-            return View (list1);
+            //// Saves the seleted records from the database to the list "listReservationDate"
+            var query = from res in _dbContextobj.Reservations.Include(p => p._resTable)
+                        where res._resArrivingTime.Date == Curr_res_arr.Date
+                        select res;
+            return View(query.ToList());
+
+            //return View (list1);
 
         }
 
