7b169c02602201fe5a8e07a5130f956c1c84a618
VisualMathijs <m.vdven1991@gmail.com>
2018-05-04 11:19:25 +0200

    Commented out the DatabaseInitialiser in the Program.cs as the database is going to change a lot

--- ASPNET_MVC_MolvenoReservationApplication/Program.cs
+++ ASPNET_MVC_MolvenoReservationApplication/Program.cs
@@ -16,24 +16,25 @@ namespace ASPNET_MVC_MolvenoReservationApplication
     {
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
+            BuildWebHost(args).Run();
+            //var host = BuildWebHost(args);
 
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<Models.MyDBContext>();
-                    DbInitializer.Initialize(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
+            //using (var scope = host.Services.CreateScope())
+            //{
+            //    var services = scope.ServiceProvider;
+            //    try
+            //    {
+            //        var context = services.GetRequiredService<Models.MyDBContext>();
+            //        DbInitializer.Initialize(context);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        var logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(ex, "An error occurred while seeding the database.");
+            //    }
+            //}
 
-            host.Run();
+            //host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
