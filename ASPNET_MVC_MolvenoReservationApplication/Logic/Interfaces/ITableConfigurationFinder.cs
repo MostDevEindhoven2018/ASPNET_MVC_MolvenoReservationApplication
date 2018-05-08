0be54a2b9dedd8562864906bd314ca250e7e63bf
VisualMathijs <m.vdven1991@gmail.com>
2018-05-08 15:09:42 +0200

    updated everything to be interface dependent. SOLID baby!
    Created a very basic version that might work, but could also set my computer on fire. No clue.

---
+++ ASPNET_MVC_MolvenoReservationApplication/Logic/Interfaces/ITableConfigurationFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNET_MVC_MolvenoReservationApplication.Models;
+
+
+namespace ASPNET_MVC_MolvenoReservationApplication.Logic
+{
+    public interface ITableConfigurationFinder
+    {
+        List<Table> GetTableConfiguration(List<Table> freeTables, int partySize);
+    }
+}
---
+++ ASPNET_MVC_MolvenoReservationApplication/Logic/TableConfiguration/FreeTableFinder.cs
@@ -0,0 +1,36 @@
+﻿using ASPNET_MVC_MolvenoReservationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_MVC_MolvenoReservationApplication.Logic
+{
+    public class FreeTableFinder : IFreeTableFinder
+    {
+        MyDBContext _context;
+
+        public FreeTableFinder(MyDBContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<Table> GetFreeTables(DateTime start, DateTime end)
+        {
+            List<Table> FreeTables;
+            List<Table> OccupiedTables;
+            List<ReservationTableCoupling> ReservationTableCouplingsInTimeslot;
+
+            // Get all ReservationTableCouplings with Reservations in our timeslot
+            ReservationTableCouplingsInTimeslot = _context.ReservationTableCouplings.Where(RTC => RTC.Reservation._resLeavingTime <= start || RTC.Reservation._resArrivingTime >= end).ToList();
+            // Then get all occupied Tables.
+            OccupiedTables = ReservationTableCouplingsInTimeslot.Select(RTC => RTC.Table).ToList();
+            // Get all tables and subtract all tables from the Couplings in the list above. 
+
+            FreeTables = _context.Tables.Where(TableFromAllTables => !OccupiedTables.Any(TableFromOccupiedTables => TableFromOccupiedTables.TableID == TableFromAllTables.TableID)).ToList();
+
+            return FreeTables;
+        }
+    }
+}
