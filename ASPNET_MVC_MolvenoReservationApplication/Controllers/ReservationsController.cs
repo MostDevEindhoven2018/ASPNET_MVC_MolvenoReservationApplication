b8889ce74f9ea91d0ea2e9713b2f054b5784d9db
irene <irene@DESKTOP-EO64GAK>
2018-05-15 16:19:33 +0200

    Updated the Edit Page so that all variables can be changed and updated,added background picture and form border

--- ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationsController.cs
+++ ASPNET_MVC_MolvenoReservationApplication/Controllers/ReservationsController.cs
@@ -28,42 +28,28 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
         // GET: Reservations
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Reservations.Include("_resGuest").ToListAsync());
+            List<Reservation> resList = new List<Reservation>();
+            resList = await _context.Reservations.Include("_resGuest").ToListAsync();
+            foreach (Reservation r in resList)
+            {
+                r.Date = r._resArrivingTime.ToShortDateString();
+                r.Time = r._resArrivingTime.ToShortTimeString();
+            }
+            return View(resList);
             //return View(await _context.Reservations.Include("_resGuest").Include("_resReservationTableCouplings").ToListAsync());
+
 
         }
 
 
         [HttpPost]
-        public IActionResult Index(IndexViewModel IndexViewModelInput)
+        public IActionResult Index(Reservation res)
         {
-            DateTime resArrivingDate = new DateTime(IndexViewModelInput.Arrivingdate.Year, IndexViewModelInput.Arrivingdate.Month, IndexViewModelInput.Arrivingdate.Day, IndexViewModelInput.ArrivingHour, IndexViewModelInput.ArrivingMinute, 0);
 
-            string dtpart = resArrivingDate.ToShortDateString();
-            string tpart = resArrivingDate.ToShortTimeString();
+            res.Date = res._resArrivingTime.ToShortDateString();
+            res.Time = res._resArrivingTime.ToShortTimeString();
 
-            Guest resGuest = new Guest()
-            {
-                _guestName = IndexViewModelInput.GuestName,
-                _guestPhone = IndexViewModelInput.GuestPhone,
-                _guestEmail = IndexViewModelInput.GuestEmail
-            };
-            Table resTable = new Table();
-
-            Reservation reservation = new Reservation()
-            {
-
-                Date = IndexViewModelInput.dtpart,
-                Time = IndexViewModelInput.tpart,
-                _resPartySize = IndexViewModelInput.Partysize,
-                _resHidePrices = IndexViewModelInput.Hideprices,
-                _resComments = IndexViewModelInput.ResComments,
-                _resGuest = resGuest
-            };
-
-
-
-            return View();
+            return View(res);
         }
 
         // GET: Reservations/Details/5
@@ -231,14 +217,13 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
             }
             return View(reservation);
         }
-
-        // POST: Reservations/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ReservationID,_resPartySize,_resArrivingTime,_resLeavingTime,_resHidePrices,_resComments,_resGuest._guestName")] Reservation reservation)
+        public async Task<IActionResult> Edit(int id, [Bind("ReservationID,_resPartySize,_resArrivingTime,_resLeavingTime,_resHidePrices,_resComments,_resGuest._guestName,_resGuest._guestEmail,_resGuest._guestPhone,_resGuest")] Reservation reservation)
         {
+
+
             if (id != reservation.ReservationID)
             {
                 return NotFound();
@@ -248,6 +233,9 @@ namespace ASPNET_MVC_MolvenoReservationApplication.Controllers
             {
                 try
                 {
+                    Guest guest = new Guest();
+                    guest = reservation._resGuest;
+                    _context.Update(guest);
                     _context.Update(reservation);
                     await _context.SaveChangesAsync();
                 }
